b6cfe55652cd1a5ae1dcfb02c417703a474f5308
Vaan525 <justicia0310@gmail.com>
2020-11-09 19:22:25 +0900

    2020-11-09-2

--- Keras/keras1109.py
+++ Keras/keras1109.py
@@ -10,9 +10,9 @@ from tensorflow.keras.layers import Dense
 
 # 2. 모델 구성
 model = Sequential() # 순차적이다.
-model.add(Dense(3, input_dim=1)) # 한개가 입력됬다
-model.add(Dense(7)) # 다음층에 5개의 요오드가 생성
-model.add(Dense(3))
+model.add(Dense(5, input_dim=1)) # 한개가 입력됬다
+model.add(Dense(5)) # 다음층에 5개의 요오드가 생성
+model.add(Dense(5))
 model.add(Dense(1)) # 결과 weight
 # 요오드의 갯수와 레이어의 깊이는 AI Developer가 정한다.
 # * Hyper Parameter Tuning *
@@ -22,13 +22,13 @@ model.add(Dense(1)) # 결과 weight
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam',
-            metrics=['acc'])
+            metrics=['accuracy'])
 
-    # mse = 손실함수는 정답에 대한 오류를 숫자로 나타내는 것으로 오답에 가까울수록 큰 값이 나온다. 반대로 정답에 가까울수록 작은 값이 나온다.
+    # loss(mse) = 손실함수는 정답에 대한 오류를 숫자로 나타내는 것으로 오답에 가까울수록 큰 값이 나온다. 반대로 정답에 가까울수록 작은 값이 나온다.
     # Optimizer(최적화) = adam
     # Metrics = 평가 지표.
 
-model.fit(x, y, epochs=1000, batch_size=1)
+model.fit(x, y, epochs=3000, batch_size=1)
     # epochs = 훈련 횟수
     # batch_size = 몇 개의 샘플로 가중치를 갱신할 것인지 지정.
 
