8212c3ee2f20fc06a00177964ce47ccbc75bf402
Vaan525 <justicia0310@gmail.com>
2020-11-10 15:04:13 +0900

    2020-11-10-6

--- Keras/2020-11-10/keras09_split2.py
+++ Keras/2020-11-10/keras09_split2.py
@@ -5,10 +5,14 @@ import numpy as np
 x = np.array(range(1, 101)) # 테스트 하고싶은 데이터
 y = np.array(range(101, 201))
 
-x_train = x[:70]
-y_train = y[:70]
-x_test = x[:30]
-y_test = y[:30]
+x_train = x[:60]
+y_train = y[:60]
+
+x_val = x[:20]
+y_val = y[:20]
+
+x_test = x[:20]
+y_test = y[:20]
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -26,15 +30,15 @@ model.add(Dense(1))
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam',
-            metrics=['mse'])
+            metrics=['acc'])
 
-model.fit(x_train, y_train, epochs=100, validation_split=0.2) 
-            #validation_data=(x_val, y_val))
+model.fit(x_train, y_train, epochs=100,  
+            validation_data=(x_val, y_val))
 
 
 # 4. 평가, 예측
 # loss, acc = model.evaluate(x, y)
-loss = model.evaluate(x_test, y_test)
+loss = model.evaluate(x_train, y_train)
 
 
 print("loss : ", loss)
@@ -42,13 +46,13 @@ print("loss : ", loss)
 
 
 # 4. 예측
-y_predict = model.predict(x_test)
+y_predict = model.predict(x_train)
 print("결과물 : \n : ", y_predict)
 
 
 # R2
 from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
+r2 = r2_score(y_train, y_predict)
 print("R2 : ", r2)
 
 
