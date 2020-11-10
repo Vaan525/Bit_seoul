f13f47df5116bd483796fc7fcdc9c555f4e875e8
Vaan525 <justicia0310@gmail.com>
2020-11-10 10:34:43 +0900

    2020-11-10

--- Keras/keras1109.py
+++ Keras/keras1109.py
@@ -1,9 +1,12 @@
+
+
 import numpy as np
 
 # 1.  데이터
 x = np.array([1,2,3,4,5])
 y = np.array([1,2,3,4,5])
-# y = ax + b
+# int64 타입
+# y = wx + b
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -28,12 +31,17 @@ model.compile(loss='mse', optimizer='adam',
     # Optimizer(최적화) = adam
     # Metrics = 평가 지표.
 
-model.fit(x, y, epochs=3000, batch_size=1)
+#model.fit(x, y, epochs=3000, batch_size=1)
+model.fit(x, y, epochs=3000)
     # epochs = 훈련 횟수
-    # batch_size = 몇 개의 샘플로 가중치를 갱신할 것인지 지정.
+    # batch_size = 몇 개의 샘플로 가중치를 갱신할 것인지 지정. 크기에 따라 훈련횟수도 달라진다. 
+    # 한번에 훈련시키는 데이터의 양
+    # default = 32
 
 # 4. 평가, 예측
-loss, acc = model.evaluate(x, y, batch_size=1)
+#loss, acc = model.evaluate(x, y, batch_size=1)
+
+loss, acc = model.evaluate(x, y)
 
 print("loss : ", loss)
 print("acc : ", acc)
